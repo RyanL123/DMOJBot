aa2ed678904cf944a6ca230e80a29e0528978559
Ryan <ryan.lichangrui@gmail.com>
2020-01-23 22:02:16 -0500

    changed status messages, cleaned up code

--- Bot.py
+++ Bot.py
@@ -1,5 +1,4 @@
 import discord
-import json
 import requests
 from config import *
 from discord.ext import commands, tasks
@@ -7,23 +6,22 @@ from discord.ext import commands, tasks
 prefix = "$"
 bot = commands.Bot(command_prefix=prefix, help_command=None)
 
+@bot.event
+async def on_read():
+    await bot.change_presence(activity=discord.Game(name="%submissions"))
 
 @bot.command()
 async def submissions(ctx, user="bartpuup", num=1):
     try:
         user_submissions = requests.get("https://dmoj.ca/api/user/submissions/" + user).json()
-        keys = user_submissions.keys()
+        keys = list(user_submissions.keys())
         for i in range(1, num+1):
-            status = user_submissions[list(keys)[-i]]['result']
-            problem = requests.get("https://dmoj.ca/api/problem/info/" + user_submissions[list(keys)[-i]]['problem']).json()["name"]
-            if user == "Davidli3100":
-                await ctx.channel.send("David is fucking gay lol <:PepeLaugh:594138680898355200>")
-            elif status == 'TLE':
-                await ctx.channel.send(user + " FUCKING TLE'd on " + problem + " LMAOOOOOO <:PepeLaugh:594138680898355200>")
-            elif status == 'WA':
-                await ctx.channel.send(user + " FUCKING WA'd on " + problem + " LMAOOOOOOOOOOOOOOOOOOOO <:PepeLaugh:594138680898355200>")
-            elif status == 'AC':
-                await ctx.channel.send(user + " AC'D on " + problem + "HOLY SHIT <:PogU:594138006999269427>")
+            status = user_submissions[keys[-i]]['result']
+            problem = requests.get("https://dmoj.ca/api/problem/info/" + user_submissions[keys[-i]]['problem']).json()["name"]
+            if status == "AC":
+                await ctx.channel.send(user + " AC'd on " + problem + " HOLY SHIT <:PogU:594138006999269427>")
+            else:
+                await ctx.channel.send(user + " FUCKING " + status + "'d on " + problem + " LMAOOOOOO <:PepeLaugh:594138680898355200>")
     except:
         await ctx.channel.send("That user does not exist")
 
