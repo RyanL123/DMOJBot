b596168e384410025e5a66f9ddd0f3f8d12c6ac6
David Li <david.li3100@gmail.com>
2020-02-24 20:11:56 -0500

    oops

--- Bot.py
+++ Bot.py
@@ -37,7 +37,7 @@ async def stats(ctx, user=None):
     if user is None:
         await ctx.channel.send(">>> **Parameters** (User)\n**User**: Name of the user on DMOJ")
         return
-    if user == "Bartpuup":
+    if user == "Bartpuup" or user == "bartpuup":
         await ctx.channel.send("Bert got 75% on a grade 11 math test lmaoooooo")
     user_submissions = None
     user_info = None
